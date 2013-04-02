380da371fe717cfd1e396d78d2d50a1434ba4821
Fábio Santos <sir.furtal@gmail.com>
2013-04-02 20:32:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Trainerlist Server
+------------------
+
+This is a server which is accessible through the REST protocol.
+
+
+Setting up
+==========
+
+Just open a command line and do
+
+        npm install
+
+To start the server, run
+
+        npm start
+
+This creates a server listening on port 8080.
+
+
+Available Urls
+==============
+
+### Trainer (also the user for now)
+
+TODO
+
+### Trainee
+
+TODO
---
+++ server.js
@@ -0,0 +1,55 @@
+/*jshint node:true*/
+'use strict';
+
+var express = require('express'),
+    app = express();
+
+var DEBUG_OOZER = {
+        _id: '1D-0F-00Z3R',
+        _rev: 'SOME-VERSION',
+        isTrainer: true,
+        email: 'oozer@email.com',
+        username: 'oozer'
+    };
+
+function respondJSON (response, object) {
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(object));
+    response.end();
+}
+
+// create user
+app.post('/trainer', function (req, res) {
+    // TODO validate required stuff when creating user
+    // TODO validate rev and id
+    var newUser = {};
+    respondJSON(res, newUser);
+});
+
+// get trainer
+app.get('/trainer/<id>', function (req, res) {
+    // TODO validate ID is id of OOZER
+    respondJSON(res, DEBUG_OOZER);
+});
+
+// edit
+app.post('/trainer/<id>', function (req, res) {
+    // TODO validate input version and id
+    DEBUG_OOZER._rev = '' + (Math.random() * 1000);
+    respondJSON(res, DEBUG_OOZER);
+});
+
+// delete
+app.post('/trainer/<id>/delete', function (req, res) {
+    // TODO validate version and id
+    respondJSON(res, {});
+});
+
+// login a user
+app.post('/user/authenticate', function (req, res) {
+    // TODO validate required params email and password are 'email@email.com' and 'password'
+    // which is OOZER's login information.
+    respondJSON(res, DEBUG_OOZER);
+});
+
+app.listen(8080);
