42dc5efab145e35c377e44b456acdab7a1994419
Fábio Santos <sir.furtal@gmail.com>
2013-04-07 14:29:06 +0100

    added first test, of the couchdb view

---
+++ test/couchdb-views.js
@@ -0,0 +1,32 @@
+var assert = require('assert'),
+    designDoc = require('../design-documents/events.js');
+    mockCouchDb = function (target) {
+            // Inject an arbitrary "emit" function into the function's closure using eval()
+            return new Function('emit, args',
+                '(' + target + ').apply({}, args);');
+        };
+
+describe('by-timestamp view', function () {
+    var viewFunc = designDoc.views['by-timestamp'];
+
+    it("Should emit timestamps as keys", function (done) {
+        var doc = {timestamp: '1234', rest: 'of stuff'},
+            emitReplacement = function (key, val) {
+                assert.equal(key, '1234');
+                assert.equal(val, doc);
+                done();
+            };
+            mockedView = mockCouchDb(viewFunc.map);
+        mockedView(emitReplacement, [doc]);
+    });
+
+    it("Should emit nothing on other documents", function (done) {
+        var doc = {nothing: 'here'};
+            emitReplacement = function () {
+                done('error: emit shouldn\'t be called');
+            },
+            mockedView = mockCouchDb(viewFunc.map);
+        mockedView(emitReplacement, [doc]);
+        done();
+    });
+});
