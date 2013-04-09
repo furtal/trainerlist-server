804d3bba086f6b54d01928abd00c8c9ea3986c06
Fábio Santos <sir.furtal@gmail.com>
2013-04-09 09:15:59 +0100

    trainer model

--- models/trainer.js
+++ models/trainer.js
@@ -1,9 +1,50 @@
-var Model = require('model.js').Model;
+var Model = require('./model.js').Model;
 
 function Trainer() {}
 
-for (member in Model.prototype) {
-    Trainer.prototype[member] = Model.prototype[member];
+Trainer.prototype = new Model();
+
+// Do some regex to validate all the fields.
+// Also make sure the required ones are there.
+Trainer.prototype.validate = function () {
+    var isValid,
+        validateAName,
+        email = this.email,
+        username = this.username,
+        firstName = this.firstName,
+        lastName = this.lastName;
+
+    // Email
+    isValid = !!email
+        && !email.match(/\s/) // no whitespace
+        && email.match(/^.+@.+\..+$/)// Has an @ sign and a dot.
+        && email.length < 255;
+
+    if (!isValid) {
+        return false;
+    }
+
+    // Username
+    isValid = !!username
+        && !username.match(/\s/) // no whitespace
+        && !username.match(/@/) // no at signs! this is how we tell if it's an username or an email
+        && username.match(/^[\w\.-_]/) // just letters, dashes and underscores
+        && username.length < 128;
+
+    if (!isValid) {
+        return false;
+    }
+
+    // firstName and lastName
+    validateAName = function (name) {
+        return !!name
+            && (!name.match(/\s/)) // no whitespace
+            && name.length < 128;
+    }
+
+    isValid = validateAName(firstName) && validateAName(lastName);
+
+    return isValid;
 }
 
 module.exports.Trainer = Trainer;
---
+++ test/trainer-model.js
@@ -0,0 +1,124 @@
+var assert = require('assert'),
+    Trainer = require('../models/trainer.js').Trainer;
+
+function trainerFactory(fields) {
+    var trainer = new Trainer();
+    fields = fields || {};
+    trainer.email = fields.email || 'a.valid@email.com';
+    trainer.username = fields.username || 'valid.username';
+    trainer.firstName = fields.firstName || 'First';
+    trainer.lastName = fields.lastName || 'Last';
+    return trainer;
+}
+
+describe('Trainer model', function () {
+    it('[testing trainerFactory defaults]', function () {
+        var trainer = trainerFactory();
+        assert.equal(trainer.validate(), true);
+    });
+    it('should validate missing fields', function () {
+        var fields = ['email', 'username', 'firstName', 'lastName'],
+            trainer,
+            fieldName,
+            result,
+            i;
+        for (i = 0; i < fields.length; i += 1) {
+            fieldName = fields[i];
+            trainer = trainerFactory();
+            delete trainer[fieldName];
+            result = trainer.validate();
+            assert.equal(result, false, 'missing ' + fieldName);
+        }
+    });
+    it('should not allow whitespace in some fields', function () {
+        var fields = ['username', 'firstName', 'lastName'],
+            trainer;
+        fields.forEach(function (fieldName) {
+            trainer = trainerFactory();
+            trainer[fieldName] = 'with space';
+            assert.equal(trainer.validate(), false);
+        });
+        // emails are different.
+        trainer = trainerFactory();
+        trainer.email = 'invalid @email.com';
+        assert.equal(trainer.validate(), false);
+    });
+    it('should validate emails', function () {
+        var trainer,
+            validEmails = [
+                'valid@email.com',
+                'valid.e_mail@their-host.com',
+                'hr@foo.fr'
+            ],
+            invalidEmails = [
+                'invalid.email@com',
+                'invalid.email',
+                'invalid@email',
+                ''
+            ];
+        validEmails.forEach(function (validEmail) {
+            trainer = trainerFactory();
+            trainer.email = validEmail;
+            assert.equal(trainer.validate(), true);
+        });
+        invalidEmails.forEach(function (invalidEmail) {
+            trainer = trainerFactory();
+            trainer.email = invalidEmail;
+            assert.equal(trainer.validate(), false);
+        });
+    });
+    it('should validate usernames', function () {
+        var trainer,
+            validNames = [
+                'joaquim',
+                'jo.a-qui_m',
+                'DirtDude127'
+            ],
+            invalidNames = [
+                'an@sign'
+            ];
+            validNames.forEach(function (validName) {
+                trainer = trainerFactory();
+                trainer.username = validName;
+                assert.equal(trainer.validate(), true, validName + ' should be valid');
+            });
+            invalidNames.forEach(function (invalidName) {
+                trainer = trainerFactory();
+                trainer.username = invalidName;
+                assert.equal(trainer.validate(), false, invalidName + ' should be invalid');
+
+            });
+    });
+    it('should validate firstName and lastName', function () {
+        var trainer,
+            validNames = [
+                'joaquim',
+                'jo.a-qui_m',
+                'DirtDude127'
+            ],
+            invalidNames = [
+                ''
+            ];
+            validNames.forEach(function (validName) {
+                trainer = trainerFactory();
+                trainer.firstName = validName;
+                assert.equal(trainer.validate(), true, validName + ' should be valid as a first name');
+            });
+            validNames.forEach(function (validName) {
+                trainer = trainerFactory();
+                trainer.lastName = validName;
+                assert.equal(trainer.validate(), true, validName + ' should be valid as a last namr');
+            });
+            invalidNames.forEach(function (invalidName) {
+                trainer = trainerFactory();
+                trainer.firstName = invalidName;
+                assert.equal(trainer.validate(), false, invalidName + ' should be invalid as first name');
+            });
+            invalidNames.forEach(function (invalidName) {
+                trainer = trainerFactory();
+                trainer.lastName = invalidName;
+                assert.equal(trainer.validate(), false, invalidName + ' should be invalid as last name');
+
+            });
+    });
+});
