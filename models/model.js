becc91db8dd8b1a400e23ac13df4a34dc7f8ea3f
Fábio Santos <sir.furtal@gmail.com>
2013-04-12 20:11:09 +0100

    created dbError, which turns couchdb error responses into errors

--- models/model.js
+++ models/model.js
@@ -2,6 +2,17 @@ var assert = require('assert'),
     JsonClient = require('request-json').JsonClient,
     fs = require('fs');
 
+function dbError(errObj) {
+    ret = new Error();
+    ret.toString = function () {
+        return 'DbError: ' + this.reason + '(' + this.error + ')';
+    };
+    ret.reason = errObj.reason;
+    ret.error = errObj.error;
+    assert(ret.reason && ret.error);
+    return ret;
+}
+
 function Model() {}
 
 Model._configDb = function (configFile, next) {
@@ -51,7 +62,7 @@ Model.prototype.save = function (next) {
         then;
     then = function (err, res, data) {
         if (err) return next(err);
-        if (data.error) return next(new Error(data.error));
+        if (data.error) return next(dbError(data));
         assert(data.id || data._id);
         assert(data.rev || data._rev);
         that._id = data.id || data._id;
@@ -81,7 +92,7 @@ Model.prototype.load = function (next) {
         that = this;
     client.get(this.getPath(), function (err, res, body) {
         if (err) return next(err);
-        if (body.error) return next(new Error(body.error));
+        if (body.error) return next(dbError(body));
         if (res.statusCode !== 200) return next('not found');
         that.extend(body);
         next(null, body);
@@ -92,8 +103,8 @@ Model.prototype.del = function (next) {
     var client = this.getClient(),
         delUrl = this.getPath() + '?rev=' + encodeURIComponent(this._rev);
     client.del(delUrl, function (err, res, body) {
-        if (body.error) return next(new Error(body.error));
         if (err) return next(err);
+        if (body.error) return next(dbError(body));
         return next(null, body);
     });
 };
